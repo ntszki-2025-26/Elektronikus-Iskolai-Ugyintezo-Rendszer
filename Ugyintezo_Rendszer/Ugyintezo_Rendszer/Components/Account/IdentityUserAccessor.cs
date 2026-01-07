80acfa2b13a1ed332eb4981bc3047853bdfeae9f
MaRtInOlOl <115780312+MaRtInOlOl@users.noreply.github.com>
2026-01-07 19:32:41 +0100

    Create_Project
    .NET 9
    Authentication type: individual acc
    render mode: server

---
+++ Ugyintezo_Rendszer/Ugyintezo_Rendszer/Components/Account/IdentityUserAccessor.cs
@@ -0,0 +1,20 @@
+using Microsoft.AspNetCore.Identity;
+using Ugyintezo_Rendszer.Data;
+
+namespace Ugyintezo_Rendszer.Components.Account
+{
+    internal sealed class IdentityUserAccessor(UserManager<ApplicationUser> userManager, IdentityRedirectManager redirectManager)
+    {
+        public async Task<ApplicationUser> GetRequiredUserAsync(HttpContext context)
+        {
+            var user = await userManager.GetUserAsync(context.User);
+
+            if (user is null)
+            {
+                redirectManager.RedirectToWithStatus("Account/InvalidUser", $"Error: Unable to load user with ID '{userManager.GetUserId(context.User)}'.", context);
+            }
+
+            return user;
+        }
+    }
+}

1841d98abafeaa34db4a0913787f815fc4c8ae32
MaRtInOlOl <115780312+MaRtInOlOl@users.noreply.github.com>
2026-02-11 12:00:53 +0100

    Models
    Modelek készen vannak a tábláknak megfelelően

---
+++ Project/EIUR/Elektronikus_Iskolai_Ugyintezo_Rendszer/Models/Role.cs
@@ -0,0 +1,12 @@
+﻿namespace Elektronikus_Iskolai_Ugyintezo_Rendszer.Models
+{
+    [Table("Roles")]   
+    public class Role
+    {
+        [Key]
+        public int Id { get; set; }
+        [Required]
+        public string RoleName { get; set; }
+
+    }
+}
---
+++ Project/EIUR/Elektronikus_Iskolai_Ugyintezo_Rendszer/Models/Subjects.cs
@@ -0,0 +1,11 @@
+﻿namespace Elektronikus_Iskolai_Ugyintezo_Rendszer.Models
+{
+    [Table("Subject")]
+    public class Subjects
+    {
+        [Key]
+        public required int Id { get; set; }
+        [Required]
+        public required string Name { get; set; }
+    }
+}
